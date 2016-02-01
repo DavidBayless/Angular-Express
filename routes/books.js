5db826d242a69cbce18363fe6cb5eaced1519f2b
David Bayless <David.C.Bayless15@gmail.com>
2016-02-01 09:47:44 -0700

    added basic API routes

--- routes/books.js
+++ routes/books.js
@@ -3,8 +3,24 @@ var knex = require('../db/knex');
 
 var router = express.Router();
 
-// Routes for /api/books/
+function books() {
+  return knex('books');
+}
 
+// Routes for /api/books/
+router.get('/', function(req, res, next) {
+  books().select()
+  .then(function(response) {
+    res.json(response);
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  books().where('id', req.params.id)
+  .then(function(response) {
+    res.json(response);
+  });
+});
 
 
 module.exports = router;
