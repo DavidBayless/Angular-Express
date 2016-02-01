1c8dfb1a47633067b7be013460969c3d47f6a763
David Bayless <David.C.Bayless15@gmail.com>
2016-02-01 10:33:43 -0700

    added cors to allow for angular calls to server

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var routes = require('./routes/books');
 
 var app = express();
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
